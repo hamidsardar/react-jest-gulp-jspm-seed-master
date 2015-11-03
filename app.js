4c63ba25de0dc7b85d7ff3606769a557071f72dd
Hamid sardar <coolsardarhamid@gmail.com>
2015-11-04 01:34:05 +0800

    Seed project for writing ReactJS components in TDD fashion. This seed uses Jest for unit testing, Gulp for building and JSPM as the browser package manager.

---
+++ app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var React = require('react');
+var CheckboxWithLabel = require('components/testComponent');
+
+React.render(<CheckboxWithLabel labelOn="On" labelOff="Off" />, document.getElementById('view'));
